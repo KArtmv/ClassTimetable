a1097de53954b3861d6d0018985c0e0b3a9cb622
”Kyrylo <kirillartemov1993@gmail.com>
2024-03-04 14:46:33 +0100

    Add methods -Add method getLessonsByTeacherAndDate -Add method getLessonsByTeacherAndDateBetween

--- src/main/java/ua/foxminded/WebProject/persistence/repository/LessonRepository.java
+++ src/main/java/ua/foxminded/WebProject/persistence/repository/LessonRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.foxminded.WebProject.persistence.entity.Group;
 import ua.foxminded.WebProject.persistence.entity.Lesson;
+import ua.foxminded.WebProject.persistence.entity.Teacher;
 
+import javax.xml.crypto.Data;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,5 +14,9 @@ import java.util.List;
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
     List<Lesson> getLessonByGroupAndDate(Group group, LocalDate date);
 
-    List<Lesson> findByGroupAndDateBetween(Group group, LocalDate firstWeekDay, LocalDate lastStudiedWeekDay);
+    List<Lesson> findByGroupAndDateBetween(Group group, LocalDate start, LocalDate ent);
+
+    List<Lesson> getLessonsByTeacherAndDate(Teacher teacher, LocalDate date);
+
+    List<Lesson> getLessonsByTeacherAndDateBetween(Teacher teacher, LocalDate start, LocalDate end);
 }
