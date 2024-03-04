5bc8830dabfdd6e705216c8b38b8bd0fecafc5a5
”Kyrylo <kirillartemov1993@gmail.com>
2024-03-04 12:12:29 +0100

    Create custom exception

---
+++ src/main/java/ua/foxminded/WebProject/exception/InvalidIdException.java
@@ -0,0 +1,8 @@
+package ua.foxminded.WebProject.exception;
+
+public class InvalidIdException extends RuntimeException {
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
+}
