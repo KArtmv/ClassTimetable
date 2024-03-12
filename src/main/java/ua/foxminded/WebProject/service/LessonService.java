8fadc79ef1fa1cfa0c48bb2705b08bf30dd2104e
”Kyrylo <kirillartemov1993@gmail.com>
2024-03-12 13:47:26 +0100

    Add new method: getAllLessonsPerDay

--- src/main/java/ua/foxminded/WebProject/service/LessonService.java
+++ src/main/java/ua/foxminded/WebProject/service/LessonService.java
@@ -4,10 +4,12 @@ import ua.foxminded.WebProject.DTO.LessonDto;
 import ua.foxminded.WebProject.persistence.entity.Lesson;
 import ua.foxminded.WebProject.persistence.entity.Student;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LessonService extends AbstractService<Lesson, Long> {
     Lesson saveLesson(LessonDto lesson);
     List<Student> getParticipantsOfLesson(Lesson lesson);
     List<Lesson> getAll();
+    List<Lesson> getAllLessonsPerDay(LocalDate date);
 }
