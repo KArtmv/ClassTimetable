c753a7afb2bb002984e44797742d299b6f05e988
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-03 10:46:32 +0200

    Add new method

--- src/main/java/ua/foxminded/WebProject/service/LessonService.java
+++ src/main/java/ua/foxminded/WebProject/service/LessonService.java
@@ -9,10 +9,8 @@ import java.util.List;
 
 public interface LessonService extends AbstractService<Lesson, Long> {
     Lesson saveLesson(LessonDto lesson);
-
     List<Student> getParticipantsOfLesson(Lesson lesson);
-
     List<Lesson> getAll();
-
     List<Lesson> getAllLessonsPerDay(LocalDate date);
+    boolean isEmpty();
 }
