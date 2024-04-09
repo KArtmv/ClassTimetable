e282a3f5c1d85c47329fe72711e6c6b23ac9e132
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-09 14:55:25 +0200

    Create script to move data from student and teacher tables  to users table

---
+++ src/main/resources/db/migration/V4__MoveStudentTeacherDataToUserTableByRoles.sql
@@ -0,0 +1,9 @@
+begin;
+
+insert into users (role, first_name, last_name, group_id)
+select 'student', first_name, last_name, group_id  from student;
+
+insert into users (role, first_name, last_name)
+select 'teacher', first_name, last_name from teacher;
+
+commit;
