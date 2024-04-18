a0f080c6dc5a01486d873c56fa125cb53ba9b321
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-18 11:30:22 +0200

    Create WebSecurityConfig -Add EnableWebSecurity annotation -Add CustomAuthenticationSuccessHandler, UserDetailsService as class member -Configure SecurityFilterChain, WebSecurityCustomizer,  DaoAuthenticationProvider, BCryptPasswordEncoder

---
+++ src/main/java/ua/foxminded/WebProject/security/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package ua.foxminded.WebProject.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import ua.foxminded.WebProject.util.CustomAuthenticationSuccessHandler;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+
+    private CustomAuthenticationSuccessHandler successHandler;
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring().requestMatchers("/css/**", "/webjars/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(authorize -> authorize
+                            .requestMatchers("/", "/login", "/login-error").permitAll()
+                            .requestMatchers("/admin").hasRole("admin")
+                            .requestMatchers("/student").hasAnyRole("student", "admin")
+                            .requestMatchers("/teacher").hasAnyRole("teacher", "admin")
+                            .requestMatchers("/course", "/group", "/lesson", "/staff").hasAnyRole("admin", "staff")
+                            .anyRequest().authenticated())
+                    .formLogin(login -> login.loginPage("/login")
+                            .successHandler(successHandler)
+                            .failureHandler(new SimpleUrlAuthenticationFailureHandler("/login-error")))
+                    .httpBasic(withDefaults())
+                    .logout(logout -> logout.permitAll().logoutSuccessUrl("/"));
+
+            httpSecurity.authenticationProvider(authenticationProvider());
+
+            return httpSecurity.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
