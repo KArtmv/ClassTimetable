08e7c9b25d1a55007976a527099f8cac5320444e
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-18 11:13:10 +0200

    Create AdminController -Add method to view admin page by request

---
+++ src/main/java/ua/foxminded/WebProject/controller/AdminController.java
@@ -0,0 +1,21 @@
+package ua.foxminded.WebProject.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+@Secured("admin")
+@AllArgsConstructor
+public class AdminController {
+
+    @GetMapping
+    public String adminPage(){
+        return "admin/admin";
+    }
+}
+
+
