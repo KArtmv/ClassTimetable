3b371fd6c760f2822ea9a7f931d9aec65f1e1d00
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-18 09:59:25 +0200

    Add AllArgsConstructor lombok annotation -Add MyLocalDate as class member -Add passing current time to home page -Add path '/' in GetMapping

--- src/main/java/ua/foxminded/WebProject/controller/HomeController.java
+++ src/main/java/ua/foxminded/WebProject/controller/HomeController.java
@@ -1,13 +1,20 @@
 package ua.foxminded.WebProject.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.foxminded.WebProject.util.MyLocalDate;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
 
-    @GetMapping
-    public String home() {
+    private MyLocalDate localDate;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("currentDate", localDate.getCurrentDate());
         return "home";
     }
 }
