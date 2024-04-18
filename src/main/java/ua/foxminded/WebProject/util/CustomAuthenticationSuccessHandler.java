882de07f9620ae2574e832b89577a7715ac45445
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-18 11:22:13 +0200

    Implement AuthenticatioSuccessHandler -Overide onAuthenticationSuccess method -Add switch to send default page by role

---
+++ src/main/java/ua/foxminded/WebProject/util/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,30 @@
+package ua.foxminded.WebProject.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+
+        String role = authentication.getAuthorities().toString();
+        role = role.substring(6, role.length() - 1);
+
+        switch (role) {
+            case "admin" -> response.sendRedirect("/admin");
+            case "teacher" -> response.sendRedirect("/teacher");
+            case "student" -> response.sendRedirect("/student");
+            case "staff" -> response.sendRedirect("/staff");
+            default -> response.sendRedirect("/");
+        }
+    }
+}
