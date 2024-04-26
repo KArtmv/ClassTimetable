90288cc752f14c2637f206de57dc0315b87a12aa
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-26 11:37:35 +0200

    Remove method checking table

--- src/main/java/ua/foxminded/WebProject/generator/LessonGenerator.java
+++ src/main/java/ua/foxminded/WebProject/generator/LessonGenerator.java
@@ -1,15 +1,14 @@
 package ua.foxminded.WebProject.generator;
 
-import jakarta.annotation.PostConstruct;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import ua.foxminded.WebProject.DTO.LessonDto;
 import ua.foxminded.WebProject.persistence.entity.*;
-import ua.foxminded.WebProject.persistence.repository.UserRepository;
 import ua.foxminded.WebProject.service.*;
 
 import java.util.List;
@@ -28,29 +27,26 @@ public class LessonGenerator {
     private final LessonService lessonService;
     private final RandomGenerator randomGenerator;
 
-    @PostConstruct
-    public void populateIfEmpty() {
-        if (lessonService.isEmpty()) {
-            populateLessonTable();
+    public void fillLessonTable() {
+        while (lessonService.getAll().size() < maxLessons) {
+            saveLesson(generateLesson());
         }
     }
 
-    private void populateLessonTable() {
+    private LessonDto generateLesson(){
         List<Group> groups = groupService.getAll();
         List<Course> courses = courseService.getAll();
         List<Teacher> teachers = teacherService.getAll();
         List<Classroom> classrooms = classroomService.getAll();
 
-        while (lessonService.getAll().size() < maxLessons) {
-            saveNewLesson(new LessonDto(
-                    courses.get(randomGenerator.nextInt(courses.size())),
-                    groups.get(randomGenerator.nextInt(groups.size())),
-                    classrooms.get(randomGenerator.nextInt(classrooms.size())),
-                    teachers.get(randomGenerator.nextInt(teachers.size()))));
-        }
+        return new LessonDto(
+                courses.get(randomGenerator.nextInt(courses.size())),
+                groups.get(randomGenerator.nextInt(groups.size())),
+                classrooms.get(randomGenerator.nextInt(classrooms.size())),
+                teachers.get(randomGenerator.nextInt(teachers.size())));
     }
 
-    private void saveNewLesson(LessonDto lessonDto) {
+    private void saveLesson(LessonDto lessonDto) {
         try {
             lessonService.saveLesson(lessonDto);
         } catch (DataIntegrityViolationException e) {
