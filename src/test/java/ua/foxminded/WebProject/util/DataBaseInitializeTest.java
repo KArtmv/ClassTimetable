a275cd4ce009d899079df409d0774f6f96a5cc42
”Kyrylo <kirillartemov1993@gmail.com>
2024-04-26 11:40:57 +0200

    Create tests for DataBaseInitialize, SqlScriptExecutor classes

---
+++ src/test/java/ua/foxminded/WebProject/util/DataBaseInitializeTest.java
@@ -0,0 +1,86 @@
+package ua.foxminded.WebProject.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import ua.foxminded.WebProject.generator.LessonGenerator;
+import ua.foxminded.WebProject.service.LessonService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class DataBaseInitializeTest {
+
+    @Mock
+    private SqlScriptExecutor scriptExecutor;
+
+    @Mock
+    private LessonGenerator lessonGenerator;
+
+    @Mock
+    private LessonService lessonService;
+
+    @Mock
+    private ApplicationArguments applicationArguments;
+
+    @InjectMocks
+    private DataBaseInitialize dataBaseInitialize;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void run_InitializeDatabaseSuccessfully() {
+        when(lessonService.isTableEmpty()).thenReturn(true);
+        when(scriptExecutor.execute()).thenReturn(true);
+
+        dataBaseInitialize.run(applicationArguments);
+
+        verify(lessonService).isTableEmpty();
+        verify(scriptExecutor).execute();
+        verify(lessonGenerator).fillLessonTable();
+    }
+
+    @Test
+    void run_DatabaseNotInitialized() {
+        when(lessonService.isTableEmpty()).thenReturn(false);
+
+        dataBaseInitialize.run(applicationArguments);
+
+        verify(lessonService).isTableEmpty();
+        verify(scriptExecutor, never()).execute();
+        verify(lessonGenerator, never()).fillLessonTable();
+    }
+
+    @Test
+    void run_ScriptExecutionFails() {
+        when(lessonService.isTableEmpty()).thenReturn(true);
+        when(scriptExecutor.execute()).thenReturn(false);
+
+        dataBaseInitialize.run(applicationArguments);
+
+        verify(lessonService).isTableEmpty();
+        verify(scriptExecutor).execute();
+        verify(lessonGenerator, never()).fillLessonTable();
+    }
+
+
+    @Test
+    void run_ScriptExecutionFails_whenThrowsException() {
+        when(lessonService.isTableEmpty()).thenReturn(true);
+        when(scriptExecutor.execute()).thenThrow(RuntimeException.class);
+
+        dataBaseInitialize.run(applicationArguments);
+
+        verify(lessonService).isTableEmpty();
+        verify(scriptExecutor).execute();
+        verify(lessonGenerator, never()).fillLessonTable();
+    }
+}
---
+++ src/test/java/ua/foxminded/WebProject/util/SqlScriptExecutorTest.java
@@ -0,0 +1,40 @@
+package ua.foxminded.WebProject.util;
+
+import org.flywaydb.test.annotation.FlywayTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SqlScriptExecutor.class}))
+@ActiveProfiles("test")
+@FlywayTest
+class SqlScriptExecutorTest {
+
+    @Autowired
+    private SqlScriptExecutor scriptExecutor;
+
+    @Test
+    void execute_ShouldReturnTrue_WhenScriptExecutesSuccessfully() {
+        assertTrue(scriptExecutor.execute());
+    }
+
+    @Test
+    void execute_ShouldReturnFalse_WhenScriptExecutesFailed() throws SQLException {
+        DataSource mockDataSource = mock(DataSource.class);
+        scriptExecutor = mock(SqlScriptExecutor.class);
+
+        when(mockDataSource.getConnection()).thenThrow(SQLException.class);
+
+        assertFalse(scriptExecutor.execute());
+    }
+}
